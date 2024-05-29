375acac847f32888e45fa1558324577a03cfb1cf
maikelele <jaskulskim.gdansk@gmail.com>
2024-05-29 11:21:06 +0200

    Learning controller and modules

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { DevicesModule } from './devices/devices.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-  imports: [DevicesModule],
+  imports: [DevicesModule, UsersModule],
   controllers: [AppController],
   providers: [AppService],
 })
